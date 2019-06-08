e215e7a0c699dbdbf31bbfe49263febd07578c10
KtoZ <32612782+KtoZ@users.noreply.github.com>
2019-06-09 02:49:01 +0900

    Remove src

--- src/app/Services/PhotoService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Services;
-
-// todo 02: Service クラスを追加
-class PhotoService implements PhotoServiceInterface
-{
-
-}
--- src/app/Services/PhotoServiceInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Services;
-
-// todo 02: Service インターフェースを追加
-interface PhotoServiceInterface
-{
-
-}
