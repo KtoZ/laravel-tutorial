530854910004e375b08168a6b92c34acb19d6009
KtoZ <32612782+KtoZ@users.noreply.github.com>
2019-06-11 21:59:43 +0900

    Update 07

--- src/tests/Unit/PhotoServiceTest.php
+++ src/tests/Unit/PhotoServiceTest.php
@@ -10,6 +10,7 @@ use Tests\TestCase;
 // todo 07: データベースユニットテストを実装
 class PhotoServiceTest extends TestCase
 {
+    // todo: 一度に動かすとエラーになる
     use RefreshDatabase;
 
     /**
@@ -20,18 +21,27 @@ class PhotoServiceTest extends TestCase
         factory(Photo::class, 3)->create();
 
         $service = new PhotoService();
-        $service->findAll();
+        $expected = $service->findAll();
 
-        $this->assertSame(3, \DB::table('photos')->count());
+        $this->assertSame(3, $expected->count());
 
     }
 
     /**
      * @test
      */
-    public function findById_データが一件だけ取得できること()
+    public function findById_データが一件取得できること()
     {
-        $this->assertTrue(true);
+        factory(Photo::class)->create([
+            'title' => 'test-title',
+            'body' => 'test-body',
+        ]);
+
+        $service = new PhotoService();
+        $expected = $service->findById(1);
+
+        $this->assertSame("test-title", $expected->title);
+        $this->assertSame("test-body", $expected->body);
     }
 
     /**
@@ -39,7 +49,13 @@ class PhotoServiceTest extends TestCase
      */
     public function insert_データが新規登録されること()
     {
-        $this->assertTrue(true);
+        $service = new PhotoService();
+        $service->insert("test-title", "test-body");
+
+        $this->assertDatabaseHas('photos', [
+            'title' => 'test-title',
+            'body' => 'test-body',
+        ]);
     }
 
     /**
@@ -47,7 +63,15 @@ class PhotoServiceTest extends TestCase
      */
     public function update_データが更新されること()
     {
-        $this->assertTrue(true);
+        factory(Photo::class)->create();
+
+        $service = new PhotoService();
+        $service->update(1, "test-title", "test-body");
+
+        $this->assertDatabaseHas('photos', [
+            'title' => 'test-title',
+            'body' => 'test-body',
+        ]);
     }
 
     /**
@@ -55,6 +79,11 @@ class PhotoServiceTest extends TestCase
      */
     public function delete_データが削除されること()
     {
-        $this->assertTrue(true);
+        factory(Photo::class)->create();
+
+        $service = new PhotoService();
+        $service->delete(1);
+
+        $this->assertSame(0, \DB::table('photos')->count());
     }
 }
